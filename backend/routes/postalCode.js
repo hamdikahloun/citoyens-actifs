b8aa29367d218b8b731f0f766a992de299b6d823
Tristan <tristan.rou2@gmail.com>
2025-06-07 12:33:31 +0200

    route coordinates ok

--- backend/routes/postalCode.js
+++ backend/routes/postalCode.js
@@ -6,7 +6,15 @@ const User = require('../models/users');
 const { checkBody } = require('../modules/checkBody');
 const uid2 = require('uid2');
 
-// route pour ajouter le code postal en BDD **********************************
+async function searchCity(city) {
+  const url = `https://nominatim.openstreetmap.org/search?city=${city}&country=France&format=json&polygon_geojson=1&limit=1`;
+  const response = await fetch(url);
+  const data = await response.json();
+  //console.log(data);
+  return data;
+}
+
+// route pour ajouter le nom de ville en BDD **********************************
 router.post("/", async (req, res) => {
   const cityName = req.body.cityName;
   const email = req.body.email;
@@ -14,17 +22,16 @@ router.post("/", async (req, res) => {
 
   try {
     //console.log(email + ' ' + cityName);
-    const url = `https://nominatim.openstreetmap.org/search?city=${cityName}&country=France&format=json&polygon_geojson=1&limit=1`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
+    const data = await searchCity(cityName);
     if (data.length && email.length > 0) {
       console.log('true');
       User.updateOne(
         { email: email },
         { city: cityName }
-      ).then(() => { console.log('ok') });
-      res.json(data);
+      ).then(() => {
+        console.log('ok');
+        res.json(data);
+      })
     } else {
       console.log('false');
       res.json('postal code not found');
@@ -34,6 +41,62 @@ router.post("/", async (req, res) => {
   }
 });
 
-// route pour récupérer les coordonnées à partir du code postal **********************************
+// route pour récupérer les limites de la ville à partir de son nom **********************************
+router.get('/city/:cityname', async function (req, res) {
+  const cityName = req.params.cityname;
+
+  if (!cityName) {
+    res.json({ result: false, error: 'cityname required' });
+  }
+  try {
+    const data = await searchCity(cityName);
+    if (!data.length || !data[0].geojson) {
+      return res.status(404).json({ error: 'Polygon not found' });
+    }
+    let polygons = [];
+    if (data[0].geojson.type === 'Polygon') {
+      polygons = [data[0].geojson.coordinates];
+      console.log('mono ' + polygons.length)
+    } else if (data[0].geojson.type === 'MultiPolygon') {
+      polygons = data[0].geojson.coordinates;
+      console.log('multi ' + polygons.length);
+    } else{
+      res.json({error: 'Polygon type unknown'});
+    }
+    const coords = polygons.flat(1)[0].map(([lng, lat])=>([lat, lng]));
+    res.json(coords);
+  } catch (err) {
+    res.status(500).json({ error: "Polygon fetch failed" });
+  }
+})
+
+// Get city polygon from coordinates
+router.get("/coordinates", async (req, res) => {
+  try {
+    console.log(req.query);
+    const { lat, lng } = req.query;
+    if (!lat || !lng) {
+      return res.status(400).json({ error: "Latitude and longitude are required" });
+    }
+
+    const url = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json&addressdetails=1`;
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent": "CitoyensActifs/1.0",
+      },
+    });
+    const data = await response.json();
+    console.log(data.address?.city);
+
+    if (!data.address?.city) {
+      return res.status(404).json({ error: "City not found for these coordinates" });
+    }
+    res.json(data.address.city);
+  }catch (err) {
+    res.status(500).json({error: 'error to find city from localisation'})
+  }
+})
+
+
 
 module.exports = router;
