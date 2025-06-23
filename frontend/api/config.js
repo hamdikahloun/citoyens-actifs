aee2097915ea46db50958592b7d6a26ffe6ee10d
Croaw <maisonneuve.thomas@gmail.com>
2025-06-23 18:02:57 +0200

    final?

--- frontend/api/config.js
+++ frontend/api/config.js
@@ -1,2 +1,2 @@
-//export const API_URL = "https://citoyens-actifs.vercel.app";
-export const API_URL = "http://192.168.1.25:3000";
+export const API_URL = "https://citoyens-actifs-theta.vercel.app";
+//export const API_URL = "http://192.168.1.25:3000";
