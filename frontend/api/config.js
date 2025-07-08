f708506958ef4d85c48a9c385c33bd16c0be4851
Croaw <maisonneuve.thomas@gmail.com>
2025-07-08 22:40:18 +0200

    reflexion

--- frontend/api/config.js
+++ frontend/api/config.js
@@ -1,3 +1,3 @@
 //export const API_URL = "https://citoyens-actifs.vercel.app";
-export const API_URL = "http://192.168.1.25:3000";
+//export const API_URL = "http://192.168.1.25:3000";
 //export const API_URL = "https://citoyens-actifs-cndj.vercel.app";
